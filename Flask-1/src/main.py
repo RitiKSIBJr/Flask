699e0ac3374387c3fc6c053fffe084fe4b97522f
RitiKSiBJr <letter.ritik060@gmail.com>
2022-12-05 13:02:54 +0545

    img

--- Flask-1/src/main.py
+++ Flask-1/src/main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import requests
 
 
 
@@ -9,11 +10,11 @@ UPLOAD_FOLDER = 'C:\\Users\\lette\\OneDrive\\Desktop\\Flask'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/index", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route("/urluploader", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def url_uploader():
     return render_template('url.html')
 
@@ -30,14 +31,17 @@ def upload():
 @app.route('/urlinput', methods=['GET', 'POST'])
 def get_url():
     if request.method == "POST":
-        url = str(request.form.get("url"))
-        if url:
-            with open('urls.txt', 'a') as file:
-                file.write(url + '\n')
-            return url
-        else:
-            return 'URL is empty'
+        url = request.form.get("url")
+        print(url)
+        
+        response = requests.get(url)
+
+        with open('img', 'wb') as f:
+            f.write(response.content)    
+        return 'Completed'
     
+    else: 
+        return 'Null'
 
 
 
