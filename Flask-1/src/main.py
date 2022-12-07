e998940de98a4185b6291613cbd5e65d33446090
RitiKSiBJr <letter.ritik060@gmail.com>
2022-12-07 16:12:21 +0545

    practice

--- Flask-1/src/main.py
+++ Flask-1/src/main.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, send_from_directory
 from werkzeug.utils import secure_filename
 import requests
-
+import hashlib
 
 
 UPLOAD_FOLDER = 'C:\\Users\\lette\\OneDrive\\Desktop\\Flask\\Flask-1\\src'
@@ -34,13 +34,19 @@ def get_url():
     if request.method == "POST":
         url = request.form.get("url")
         response = requests.get(url)  
+        extension =  "."+ url.split("/")[-1:][0].split(".")[-1:][0]
 
+        filename = hashlib.md5(url.encode('utf-8')).hexdigest()
 
-        with open('src/static/fb.ico', 'wb+') as f:
+        if os.path.exists("src/static/"+ filename + extension):
+            return send_from_directory(dir,path='static/'+filename+extension)
+
+        with open('src/static/'+filename+extension, 'wb+') as f:
             f.write(response.content)
+            return 'completed'
            
         
-        return send_from_directory(dir,path='static/img')
+        
 
     else: 
         return 'Null'
